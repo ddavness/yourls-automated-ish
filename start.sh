a99c91c4f18c947a519b50519c4c87274fbd0290
David Duque <david.f.s.duque@tecnico.ulisboa.pt>
2020-07-01 16:02:26 +0100

    First script iteration

--- start.sh
+++ start.sh
@@ -1,4 +1,14 @@
+#!/usr/bin/env bash
+
 # Basic, automatic configuration of YOURLS
+
+# Got root?
+if [[ $EUID -ne 0 ]]; then
+    echo "ERROR: Please run me as root!"
+    exit 1
+fi
+
+START=$(pwd)
 
 # Lock these files for now
 chown root:root config-template.php
@@ -8,11 +18,11 @@ chmod 600 mariadb-init.sql
 
 apt update
 apt install -y \
-            php php-fpm php-cli php-mysql php-zip \ # PHP Dependencies
+            php php-fpm php-cli php-mysql php-zip \
             php-gd php-mbstring php-curl php-xml \
             php-pear php-bcmath \
-            mariadb-server mariadb-client \ # MariaDB/MySql
-            openssl sed git # These are generally already installed but it's never a bad idea to make sure
+            mariadb-server mariadb-client \
+            openssl sed jq unzip wget
 
 echo "Generating system secrets..."
 DB_PASSWORD=$(dd if=/dev/urandom bs=4096 count=1024 2> /dev/null |\
@@ -27,16 +37,68 @@ sed -i "s/___DATABASE_PASSWORD___/${DB_PASSWORD}/g" mariadb-init.sql
 sed -i "s/___DATABASE_PASSWORD___/${DB_PASSWORD}/g" config-template.php
 sed -i "s/___COOKIEKEY___/${COOKIE}/g" config-template.php
 
-echo "Initializing databse..."
+# Fork the template and lock it
+rm -f config.php
+cp config-template.php config.php
+chown root:root config.php
+chmod 600 config.php
 
+YOURLS_VERSION=$(curl --silent "https://api.github.com/repos/YOURLS/YOURLS/releases/latest" | jq -r .tag_name)
+echo "Downloading YOURLS, version ${YOURLS_VERSION}"
+rm -f "${YOURLS_VERSION}.zip"
+wget "https://github.com/YOURLS/YOURLS/archive/${YOURLS_VERSION}.zip"
+
+echo "Initializing databse..."
 mysql < mariadb-init.sql
 
-echo "Importing YOURLS and updating to latest released version..."
-git pull --recurse-submodules
-cd yourls
-git pull origin master
-git checkout $(git describe --tags --abbrev=0)
-echo "YOURLS VERSION: $(git describe --tags --abbrev=0)"
-cd ..
-
 # WIP - User configuration here
+echo "We're now going to configure the first user."
+echo -n "Please input your username: "
+read USER
+echo -n "Please input your password: "
+read -s PASSWORD_1
+echo -n "(again): "
+read -s PASSWORD_2
+
+while [[ "${PASSWORD_1}" -ne "${PASSWORD_2}" ]]; do
+    echo "Password mismatch! Please try again."
+    echo -n "Please input your password: "
+    read -s PASSWORD_1
+    echo -n "(again): "
+    read -s PASSWORD_2
+done
+
+sed -i "s/___USERNAME___/${USER}/g" config.php
+sed -i "s/___USERPASSWORD___/${PASSWORD_1}/g" config.php
+
+echo -n "Where do you want to unpack YOURLS? Specify a directory: "
+read INSTALL_DIR
+
+while [ ! -d ${INSTALL_DIR} ]; do
+    echo -n "This is not a directory! Please specify a directory: "
+    read INSTALL_DIR
+done
+
+"Unpacking into ${INSTALL_DIR}..."
+
+rm -f "${INSTALL_DIR}/yourls.zip"
+mv "${YOURLS_VERSION}.zip" "${INSTALL_DIR}/yourls.zip"
+cd ${YOURLS_VERSION}
+unzip yourls.zip
+rm -f yourls.zip
+mv YOURLS-${YOURLS_VERSION}/* .
+rm -rf YOURLS-${YOURLS_VERSION}/
+
+# Copy our template there and last minute patches
+mv ${START}/config.php user/config.php
+rm user/config-sample.php
+mv sample-robots.txt robots.txt
+
+chmod 666 user/config.php
+
+echo "YOU'LL STILL NEED TO CONFIGURE YOUR WEBSERVER TO SERVE THE YOURLS DIRECTORY (NGINX/APACHE/WHATEVER)"
+echo "ONCE YOU'RE DONE AND MANAGE TO LOG IN SUCCESSFULLY INTO THE ADMIN FOR THE FIRST TIME, PLEASE RUN THIS COMMAND:"
+echo ""
+echo "sudo chmod 644 $(pwd)/user/config.php"
+
+cd ${START}
